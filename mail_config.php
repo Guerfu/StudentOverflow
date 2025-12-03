8ab33236951d58699729e6ee3ac2f653fc8eb102
Hau Nguyen <tonytech0909@gmail.com>
2025-12-03 09:48:55 +0700

    Update mail_config.php

--- mail_config.php
+++ mail_config.php
@@ -1,14 +1,15 @@
 <?php
-// C:\xampp\htdocs\STUDENTOVERFLOW\mail_config.php
+// STUDENTOVERFLOW\mail_config.php
 $mailCfg = [
   'host'       => 'smtp.gmail.com',
   'port'       => 587,
   'secure'     => 'tls',
   'smtp_auth'  => true,
-  'user'       => 'hauntgcc240106@gmail.com', // the SMTP account you send with
-  'pass'       => 'uvuh bdoj iskq kehu', // <-- paste the Gmail App Password here
+  'user'       => 'hauntgcc240106@gmail.com', // SMTP account 
+  'pass'       => 'uvuh bdoj iskq kehu', // 
   'from_email' => 'hauntgcc240106@gmail.com',  // authenticated sender
   'from_name'  => 'StudentOverflow',
-  'to_email'   => 'hauntgcc240106@gmail.com',  // admin inbox (can be same)
+  'to_email'   => 'hauntgcc240106@gmail.com',  // admin inbox 
   'to_name'    => 'Site Admin',
 ];
+
